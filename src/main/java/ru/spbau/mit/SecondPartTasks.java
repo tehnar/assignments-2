66471c27eab1eebae3222a7597576f8b805e0e07
tehnar <tehnar5@gmail.com>
2016-02-18 12:47:13 +0300

    Second HW completed

--- src/main/java/ru/spbau/mit/SecondPartTasks.java
+++ src/main/java/ru/spbau/mit/SecondPartTasks.java
@@ -1,7 +1,11 @@
 package ru.spbau.mit;
 
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class SecondPartTasks {
 
@@ -9,25 +13,52 @@ public final class SecondPartTasks {
 
     // Найти строки из переданных файлов, в которых встречается указанная подстрока.
     public static List<String> findQuotes(List<String> paths, CharSequence sequence) {
-        throw new UnsupportedOperationException();
+        return paths
+                .stream()
+                .flatMap(path -> {
+                    try {
+                        return Files.lines(Paths.get(path));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        return null;
+                    }
+                })
+                .filter(s -> s.contains(sequence))
+                .collect(Collectors.toList());
     }
 
     // В квадрат с длиной стороны 1 вписана мишень.
     // Стрелок атакует мишень и каждый раз попадает в произвольную точку квадрата.
     // Надо промоделировать этот процесс с помощью класса java.util.Random и посчитать, какова вероятность попасть в мишень.
+    private static final long ITER_CNT = 10000000;
+    private static final double RADIUS = 0.5;
+    private static final Random RNG = new Random(123);
+
     public static double piDividedBy4() {
-        throw new UnsupportedOperationException();
+        return Stream.generate(() -> Math.pow(RNG.nextDouble() - RADIUS, 2) + Math.pow(RNG.nextDouble() - RADIUS, 2))
+                .limit(ITER_CNT)
+                .mapToDouble(dist -> dist <= Math.pow(RADIUS, 2) ? 1 : 0)
+                .average()
+                .getAsDouble();
     }
 
     // Дано отображение из имени автора в список с содержанием его произведений.
     // Надо вычислить, чья общая длина произведений наибольшая.
     public static String findPrinter(Map<String, List<String>> compositions) {
-        throw new UnsupportedOperationException();
+        return compositions
+                .entrySet()
+                .stream()
+                .max(Comparator.comparing(entry -> entry.getValue().stream().collect(Collectors.summingLong(String::length))))
+                .map(Map.Entry::getKey)
+                .orElse(null);
     }
 
     // Вы крупный поставщик продуктов. Каждая торговая сеть делает вам заказ в виде Map<Товар, Количество>.
     // Необходимо вычислить, какой товар и в каком количестве надо поставить.
     public static Map<String, Integer> calculateGlobalOrder(List<Map<String, Integer>> orders) {
-        throw new UnsupportedOperationException();
+        return orders
+                .stream()
+                .flatMap(map -> map.entrySet().stream())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (lhs, rhs) -> lhs + rhs));
     }
 }
--- src/test/java/ru/spbau/mit/SecondPartTasksTest.java
+++ src/test/java/ru/spbau/mit/SecondPartTasksTest.java
@@ -2,27 +2,74 @@ package ru.spbau.mit;
 
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+import static ru.spbau.mit.SecondPartTasks.*;
 
 public class SecondPartTasksTest {
 
     @Test
     public void testFindQuotes() {
-        fail();
+        List<String> answersFirst = Arrays.asList(
+                "abacaba",
+                "abacabadabacaba",
+                "acacaba //there is a small typo in 'abacaba' word",
+                " abacaba ");
+
+        List<String> answersSecond = Arrays.asList(
+                "Not a string from ABBA's 'money' song",
+                "Money, money, money",
+                "Money, money, money",
+                "If I had a little money",
+                "Money, money, money",
+                "Money, money, money"
+        );
+
+        List<String> paths = Arrays.asList("src/test/resources/abacabas.txt", "src/test/resources/abba.txt");
+        assertEquals(answersFirst, findQuotes(paths, "abacaba"));
+        assertEquals(answersSecond, findQuotes(paths, "money"));
+        assertEquals(new ArrayList<String>(), findQuotes(new ArrayList<>(), ""));
     }
 
     @Test
     public void testPiDividedBy4() {
-        fail();
+        assertEquals(Math.PI / 4, piDividedBy4(), 1e-4);
     }
 
     @Test
     public void testFindPrinter() {
-        fail();
+        Map<String, List<String>> compositions = new HashMap<>();
+        compositions.put("abc", Arrays.asList("abc", "de", "fgh"));
+        compositions.put("xyz", Arrays.asList("abc", "defgh"));
+        compositions.put("answer", Arrays.asList("abcdefghi"));
+        assertEquals("answer", findPrinter(compositions));
+        assertEquals(null, findPrinter(new HashMap<>()));
+        compositions.put("newAnswer", Arrays.asList("", "", "", "", "a", "veryLongString"));
+        assertEquals("newAnswer", findPrinter(compositions));
     }
 
     @Test
     public void testCalculateGlobalOrder() {
-        fail();
+        Map<String, Integer> firstMap = new HashMap<>(), secondMap = new HashMap<>(), thirdMap = new HashMap<>();
+        firstMap.put("banana", 3);
+        firstMap.put("apple", 5);
+        firstMap.put("mushrooms", 239);
+
+        secondMap.put("banana", 123);
+        secondMap.put("apple", 1);
+
+        thirdMap.put("mushrooms", 1);
+        thirdMap.put("test", 10);
+
+        Map<String, Integer> answer1 = new HashMap<>();
+        answer1.put("banana", 123 + 3);
+        answer1.put("apple", 5 + 1);
+        answer1.put("mushrooms", 1 + 239);
+        answer1.put("test", 10);
+
+        assertEquals(answer1, calculateGlobalOrder(Arrays.asList(firstMap, secondMap, thirdMap)));
+        assertEquals(firstMap, calculateGlobalOrder(Arrays.asList(firstMap)));
+        assertEquals(new HashMap<>(), calculateGlobalOrder(Arrays.asList(new HashMap<String, Integer>())));
     }
 }
