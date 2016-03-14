3d269e0c0b3b2a92b662fe03d93206e73c987283
tehnar <tehnar5@gmail.com>
2016-03-14 16:37:47 +0300

    Codestyle fixed

--- src/test/java/ru/spbau/mit/TestFTP.java
+++ src/test/java/ru/spbau/mit/TestFTP.java
@@ -11,14 +11,18 @@ import static org.junit.Assert.assertEquals;
 
 public class TestFTP {
     private static final String TEST_FOLDER = "src/test/resources/testFolder";
+    private static final int TEST_PORT = 4444;
+
     @Test
     public void simpleTestList() throws IOException {
-        FTPServer server = new FTPServer(4444, TEST_FOLDER);
-        FTPClient client = new FTPClient("localhost", 4444);
+        FTPServer server = new FTPServer(TEST_PORT, TEST_FOLDER);
+        FTPClient client = new FTPClient("localhost", TEST_PORT);
         assertEquals(Arrays.asList(".\\", "123.txt", "innerFolder\\"), client.listFiles("."));
-        assertEquals(Arrays.asList("innerInnerFolder\\", "234.txt"), client.listFiles("innerFolder\\innerInnerFolder"));
+        assertEquals(Arrays.asList("innerInnerFolder\\", "234.txt"),
+                client.listFiles("innerFolder\\innerInnerFolder"));
         client.getAndSaveFile("123.txt", ".");
-        assertEquals(Files.readAllLines(Paths.get(TEST_FOLDER + "/123.txt")), Files.readAllLines(Paths.get("123.txt")));
+        assertEquals(Files.readAllLines(Paths.get(TEST_FOLDER + "/123.txt")),
+                Files.readAllLines(Paths.get("123.txt")));
         server.close();
         client.close();
     }
