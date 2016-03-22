52d6a86bf9cae367b9a0ceaff8c9696c66a9ab6d
tehnar <tehnar5@gmail.com>
2016-03-22 11:27:58 +0300

    Checkstyle fixes

--- src/test/java/ru/spbau/mit/TestFTP.java
+++ src/test/java/ru/spbau/mit/TestFTP.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import static org.junit.Assert.assertArrayEquals;
