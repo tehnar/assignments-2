c2429af5c36f2468a7f5b6369b9dc0356a200b97
tehnar <tehnar5@gmail.com>
2016-03-26 16:11:38 +0300

    Forgot to fix one checkstyle error

--- src/test/java/ru/spbau/mit/TestThreadExpectedException.java
+++ src/test/java/ru/spbau/mit/TestThreadExpectedException.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(Enclosed.class)
-public class TestThreadExpectedException {
+public final class TestThreadExpectedException {
     private TestThreadExpectedException() {
     }
 
